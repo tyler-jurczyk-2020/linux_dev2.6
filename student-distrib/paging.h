cfc781707b65889631b818ac348af6247fefce35
tyler-jurczyk-2020 <tylerjurczyk2020@gmail.com>
2023-10-12 21:45:38 -0500

    Inital Paging Implementation

---
+++ student-distrib/paging.h
@@ -0,0 +1,12 @@
+#ifndef _PAGING
+#define _PAGING
+
+#define FOUR_KB 4096
+#define TABLE_SZ 1024
+#define PG_TBL_IDX 184
+
+uint32_t page_dir[TABLE_SZ] __attribute__((aligned(4096)));
+
+void setup_pager_directory();
+
+#endif
