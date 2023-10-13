09f93db918f872c546ce92798903857463f9d224
tyler-jurczyk-2020 <tylerjurczyk2020@gmail.com>
2023-10-13 18:02:50 -0500

    Setup paging properly; Struct now defined for paging entries

--- student-distrib/paging.c
+++ student-distrib/paging.c
@@ -1,49 +1,32 @@
 #include "types.h"
 #include "paging.h"
 
-uint32_t page_tbl[TABLE_SZ] __attribute__((aligned(4096)));
 
-/* # Declare page table for 0-4MB
- * .align 4096
- * page_tbl:
- * .fill VIDEO_ENT-1, 4, 0     
- * # Only Present set. Need Global?
- * .long 0x000B8001 # 0x000B8000
- * .fill 1024-VIDEO_ENT, 4, 0
- */
 void setup_pager_table() {
     unsigned int i;
     for(i = 0; i < TABLE_SZ; i++) {
         if (i == PG_TBL_IDX) {
-            page_tbl[i] =  0x000B8003;
+            page_tbl[i].entry =  VIDEO_PAGE_FLAGS;
         } 
         else {
-            page_tbl[i] = 0x02;
+            page_tbl[i].entry = EMPTY_PAGE;
         }
     }   
 }
-/* # Declare a page directory with 1024 entries
- * .align 4096
- * page_dir:
- * # Split into 4kB sectors
- * .long page_tbl+1
- * # 4 MB Kernel, set Global, Page Size, Present
- * # Defaults to Supervisor
- * .long 0x00400181 # 0x00400000
- * .fill 1022, 4, 0 # Not present
- */
+
+
 void setup_pager_directory() {
     setup_pager_table();
     unsigned int i;
     for(i = 0; i < TABLE_SZ; i++) {
         if (i == 0) {
-            page_dir[i] = ((uint32_t) page_tbl) | 3; // Sets the present and r/w priv
+            page_dir[i].kb.entry = ((uint32_t) page_tbl) | 3; // Sets the present and r/w priv
         }
         else if (i == 1) {
-            page_dir[i] = 0x00400183;
+            page_dir[i].mb.entry = KERNEL_PAGE_FLAGS;
         }
         else {
-            page_dir[i] = 0x02;
+            page_dir[i].empty = EMPTY_PAGE;
         }
     }
 }
--- student-distrib/paging.h
+++ student-distrib/paging.h
@@ -1,11 +1,85 @@
 #ifndef _PAGING
 #define _PAGING
 
+#include "types.h"
+
 #define FOUR_KB 4096
 #define TABLE_SZ 1024
 #define PG_TBL_IDX 184
+#define VIDEO_PAGE_FLAGS 0x000B8003
+#define KERNEL_PAGE_FLAGS 0x00400183
+#define EMPTY_PAGE 0x02
 
-uint32_t page_dir[TABLE_SZ] __attribute__((aligned(4096)));
+typedef struct {
+    union {
+        uint32_t entry;
+        struct {
+            uint32_t present       : 1;
+            uint32_t r_w           : 1;
+            uint32_t usr_supr      : 1;
+            uint32_t write_thru    : 1;
+            uint32_t disable_cache : 1;
+            uint32_t accessed      : 1;
+            uint32_t reserved      : 1;
+            uint32_t page_size     : 1;
+            uint32_t global        : 1;
+            uint32_t avail         : 3;
+            uint32_t base_addr     : 20;
+            } __attribute__ ((packed));
+    };
+} page_directory_entry_kb_t;
+
+typedef struct {
+    union {
+        uint32_t entry;
+        struct {
+            uint32_t present       : 1;
+            uint32_t r_w           : 1;
+            uint32_t usr_supr      : 1;
+            uint32_t write_thru    : 1;
+            uint32_t disable_cache : 1;
+            uint32_t accessed      : 1;
+            uint32_t dirty         : 1;
+            uint32_t page_size     : 1;
+            uint32_t global        : 1;
+            uint32_t avail         : 3;
+            uint32_t tbl_attr_idx  : 1;
+            uint32_t reserved      : 9;
+            uint32_t base_addr     : 10;
+            } __attribute__ ((packed));
+    };
+} page_directory_entry_mb_t;
+
+typedef struct {
+    union {
+        page_directory_entry_kb_t kb;
+        page_directory_entry_mb_t mb;       
+        uint32_t empty;
+    };
+} page_directory_entry_t;
+
+typedef struct {
+    union {
+        uint32_t entry;
+        struct {
+            uint32_t present       : 1;
+            uint32_t r_w           : 1;
+            uint32_t usr_supr      : 1;
+            uint32_t write_thru    : 1;
+            uint32_t disable_cache : 1;
+            uint32_t accessed      : 1;
+            uint32_t dirty         : 1;
+            uint32_t tbl_attr_idx  : 1;
+            uint32_t global        : 1;
+            uint32_t avail         : 3;
+            uint32_t base_addr     : 20;
+            } __attribute__ ((packed));
+    };
+} page_table_entry_t;
+
+page_directory_entry_t page_dir[TABLE_SZ] __attribute__((aligned(FOUR_KB)));
+
+page_table_entry_t page_tbl[TABLE_SZ] __attribute__((aligned(FOUR_KB)));
 
 void setup_pager_directory();
 
